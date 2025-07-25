182dd1a3152f42de3979e500f9d129b16bbd83ff
hema325 <hema24790@gmail.com>
2025-07-25 19:06:54 +0300

    remove unnecessary files in the project

--- Katiba55.Client/src/app/app.routes.ts
+++ Katiba55.Client/src/app/app.routes.ts
@@ -43,38 +43,6 @@ export const routes: Routes = [
         loadChildren: () => import('./views/medias/routes').then((m) => m.routes)
       },
       {
-        path: 'theme',
-        loadChildren: () => import('./views/theme/routes').then((m) => m.routes)
-      },
-      {
-        path: 'base',
-        loadChildren: () => import('./views/base/routes').then((m) => m.routes)
-      },
-      {
-        path: 'buttons',
-        loadChildren: () => import('./views/buttons/routes').then((m) => m.routes)
-      },
-      {
-        path: 'forms',
-        loadChildren: () => import('./views/forms/routes').then((m) => m.routes)
-      },
-      {
-        path: 'icons',
-        loadChildren: () => import('./views/icons/routes').then((m) => m.routes)
-      },
-      {
-        path: 'notifications',
-        loadChildren: () => import('./views/notifications/routes').then((m) => m.routes)
-      },
-      {
-        path: 'widgets',
-        loadChildren: () => import('./views/widgets/routes').then((m) => m.routes)
-      },
-      {
-        path: 'charts',
-        loadChildren: () => import('./views/charts/routes').then((m) => m.routes)
-      },
-      {
         path: 'pages',
         loadChildren: () => import('./views/pages/routes').then((m) => m.routes)
       }
@@ -94,12 +62,12 @@ export const routes: Routes = [
       title: 'Page 500'
     }
   },
-  {
-    path: 'login',
-    loadComponent: () => import('./views/pages/login/login.component').then(m => m.LoginComponent),
-    data: {
-      title: 'تسجيل الدخول'
-    }
-  },
+  // {
+  //   path: 'login',
+  //   loadComponent: () => import('./views/pages/login/login.component').then(m => m.LoginComponent),
+  //   data: {
+  //     title: 'تسجيل الدخول'
+  //   }
+  // },
   { path: '**', redirectTo: '/404' }
 ];
